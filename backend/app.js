66184d9cb596e0d55cf182b81eea87596cc49a9d
DUCHNH <hduy01012000@gmail.com>
2023-03-16 14:28:22 +0700

    stage 0.1.0 finished

--- backend/app.js
+++ backend/app.js
@@ -1,84 +1,41 @@
-const express = require('express');
-const bodyParser = require('body-parser')
-const mongoose = require('mongoose')
-const Post = require('./models/post')
+const express = require("express");
+const bodyParser = require("body-parser");
+const mongoose = require("mongoose");
 
-const uri = "mongodb+srv://johndoe:123@cluster0.busdn8d.mongodb.net/node-angular?retryWrites=true&w=majority";
+const postsRoutes = require("./routes/posts");
 
 const app = express();
 
-mongoose.connect(uri).then(() => {
-  console.log('Connected to database');
-}).catch(() => {
-  console.log('Connection failed');
-})
 
-app.use(bodyParser.json())
-app.use(bodyParser.urlencoded({ extended: false }))
+const uri = "mongodb+srv://johndoe:123@cluster0.busdn8d.mongodb.net/node-angular?retryWrites=true&w=majority";
 
+mongoose
+  .connect(
+    uri
+  )
+  .then(() => {
+    console.log("Connected to database!");
+  })
+  .catch(() => {
+    console.log("Connection failed!");
+  });
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
 
 app.use((req, res, next) => {
-  res.setHeader("Access-Control-Allow-Origin", "*")
-  res.setHeader("Access-Control-Allow-Headers", "Origin, X-Requested, Content-Type,Accept")
-  res.setHeader("Access-Control-Allow-Methods", "GET, POST, PATCH, DELETE, PUT, OPTIONS")
-  next()
-})
-
-app.use((req, res, next) => {
-  next()
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept"
+  );
+  res.setHeader(
+    "Access-Control-Allow-Methods",
+    "GET, POST, PATCH, PUT, DELETE, OPTIONS"
+  );
+  next();
 });
 
-app.post('/api/posts', (req, res, next) => {
-  const post = new Post({
-    title: req.body.title,
-    content: req.body.content
-  });
-  post.save().then(createdPost => {
-    res.status(201).json({
-      message: 'Post added successfully',
-      postId: createdPost._id
-    })
-  });
-
-})
-
-app.get('/api/posts', (req, res, next) => {
-  Post.find().then(document => {
-    console.log(document)
-    res.status(200).json({
-      message: 'Posts fetched successfully',
-      posts: document
-    });
-  });
-});
-
-app.get('/api/posts/:id', (req, res, next) => {
-  Post.findById(req.params.id).then(post => {
-    if (post) {
-      res.status(200).json(post)
-    } else {
-      res.status(404).json({ message: 'Post not found !!!' })
-    }
-  });
-});
-
-app.delete('/api/posts/:id', (req, res, next) => {
-  Post.deleteOne({ _id: req.params.id }).then(result => {
-    console.log(result);
-    res.status(200).json({ message: 'Deleted item' })
-  })
-})
-
-app.put('/api/posts/:id', (req, res, next) => {
-  const post = new Post({
-    _id: req.body.id,
-    title: req.body.title,
-    content: req.body.content
-  })
-
-  Post.updateOne({ _id: req.params.id }, post).then(result => {
-    res.status(200).json({ message: 'Update successful !' })
-  })
-})
+app.use("/api/posts", postsRoutes);
 
 module.exports = app;
